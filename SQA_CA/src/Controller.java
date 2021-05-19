8e084584a3c78f5730b666869728131c9672abb9
chloedunne <chloedunne1@gmail.com>
2021-05-19 11:23:04 +0100

    Method and unit test created for creating a Rubric

--- SQA_CA/src/Controller.java
+++ SQA_CA/src/Controller.java
@@ -1,4 +1,43 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Controller {
+	
+	private ArrayList<Rubric> rubricList = new ArrayList<Rubric>();
+
+	// Create Rubric
+	public Rubric createRubric(String name, ArrayList<String> criterion, ArrayList<StudentGrade> studentGrades) {
+
+		if (criterion != null) {
+			if (criterion.size() > 10) {
+				System.out.println("More than 10 criterion");
+				return null;
+			}
+		}
+
+		if (studentGrades != null) {
+			for (StudentGrade studentGrade : studentGrades) {
+				if (!checkGrades(studentGrade.getGrade())) {
+					return null;
+				}
+			}
+		}
+
+		Rubric rubric = new Rubric(name, criterion, studentGrades);
+		rubricList.add(rubric);
+		return rubric;
+	}
+
+	// Check if grades are between 1-5
+	public boolean checkGrades(HashMap<String, Integer> grades) {
+
+		for (Integer i : grades.values()) {
+			if (i > 5 || i < 1) {
+				System.out.println("Grade is not between 1 - 5");
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
--- SQA_CA/src/UnitTest.java
+++ SQA_CA/src/UnitTest.java
@@ -1,4 +1,49 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
 
 public class UnitTest {
+
+	private Controller controller;
+
+	@Test
+	public void testCreateRubric() {
+
+		controller = new Controller();
+
+		ArrayList<String> criterion = new ArrayList<>();
+
+		criterion.add(new String("Design"));
+		criterion.add(new String("Implementation"));
+		criterion.add(new String("Testing"));
+
+		Rubric rubric = controller.createRubric("Rubric", criterion, null);
+
+		assertEquals("Rubric", rubric.getName());
+		assertEquals(criterion, rubric.getCriterion());
+		assertEquals(null, rubric.getGrades());
+
+		//Testing rubric with more than 10 criteria will return null
+		ArrayList<String> criterion1 = new ArrayList<>();
+
+		criterion1.add(new String("Test1"));
+		criterion1.add(new String("Test2"));
+		criterion1.add(new String("Test3"));
+		criterion1.add(new String("Test4"));
+		criterion1.add(new String("Test5"));
+		criterion1.add(new String("Test6"));
+		criterion1.add(new String("Test7"));
+		criterion1.add(new String("Test8"));
+		criterion1.add(new String("Test9"));
+		criterion1.add(new String("Test10"));
+		criterion1.add(new String("Test11"));
+
+		Rubric rubric1 = controller.createRubric("Test Rubric", criterion1, null);
+
+		assertEquals(null, rubric1);
+
+	}
 
 }
